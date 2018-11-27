ea3e0a2e7508cca040ba906988d826b9afe02428
sarveshkalwit <44657604+sarveshkalwit@users.noreply.github.com>
2018-11-26 21:37:59 -0600

    Updated main.js
    Added a time buffer

--- main.js
+++ main.js
@@ -4,6 +4,11 @@ let poseNet;
 let poses = [];
 let skeletons = [];
 let minConfidence = 0.75;
+let oneTimeTasks = 0;
+let frames = 0;
+let num = 0;
+let myMovement;
+
 
 function setup() {
   var cnv = createCanvas(640, 480);
@@ -29,17 +34,39 @@ function modelReady() {
 function draw() {
   image(video, 0, 0, width, height);
 
+
+
+
   // We can call both functions to draw all keypoints and the skeletons
-   drawKeypoints();
+  drawKeypoints();
   // drawSkeleton();
   // printKeypoints();
-   // console.log(model_getPoseData());
-   // console.log(determineMovement());
-   // console.log(model_getPartCoordinate("nose", minConfidence));
-   if (bodyPosIsValid()) {
-     determineMovement();
-   }
+  // console.log(model_getPoseData());
+  // console.log(determineMovement());
+  // console.log(model_getPartCoordinate("nose", minConfidence));
+
+
+  if (oneTimeTasks < 200) {
+    if (bodyPosIsValid()) {
+      num += determineMovement() + 0.0;
+
+
+      oneTimeTasks++;
+    }
+    else {
+      console.log("invalid");
+    }
+  }
+  else {
+    //everything else
+    console.log(determineMovement());
+
+  }
+
+
+  // if (oneTimeTasks >= 1000) {
+  //   //open a new html page, pass it
+  // }
+
+
 }
-
-
-
